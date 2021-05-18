0bb98de4fcc8bf68234a3305f43e5846ac9c9dbf
pranac_accionlabs <pranav.patil@aacionlabs.com>
2021-05-18 12:55:50 +0530

    removed Environmental  aspect

--- server/createExpressHandler.ts
+++ server/createExpressHandler.ts
@@ -4,24 +4,20 @@ import { ServerlessContext, ServerlessFunction } from './types';
 import Twilio from 'twilio';
 
 const {
-  TWILIO_ACCOUNT_SID,
-  TWILIO_API_KEY_SID,
-  TWILIO_API_KEY_SECRET,
-  TWILIO_CONVERSATIONS_SERVICE_SID,
   REACT_APP_TWILIO_ENVIRONMENT,
 } = process.env;
 
-const twilioClient = Twilio(TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, {
-  accountSid: TWILIO_ACCOUNT_SID,
+const twilioClient = Twilio('SK18233076d87af15cacbd81bafffc9dc6', '6jX9cNLTBwkDUpeRVmreat5uxVCfm2Ry', {
+  accountSid: 'AC7212e3ea5d4204d27e2a782fd05950e5',
   region: REACT_APP_TWILIO_ENVIRONMENT === 'prod' ? undefined : REACT_APP_TWILIO_ENVIRONMENT,
 });
 
 const context: ServerlessContext = {
-  ACCOUNT_SID: TWILIO_ACCOUNT_SID,
-  TWILIO_API_KEY_SID,
-  TWILIO_API_KEY_SECRET,
+  ACCOUNT_SID: 'AC7212e3ea5d4204d27e2a782fd05950e5',
+  TWILIO_API_KEY_SID:'SK18233076d87af15cacbd81bafffc9dc6',
+  TWILIO_API_KEY_SECRET:'6jX9cNLTBwkDUpeRVmreat5uxVCfm2Ry',
   ROOM_TYPE: 'group',
-  CONVERSATIONS_SERVICE_SID: TWILIO_CONVERSATIONS_SERVICE_SID,
+  CONVERSATIONS_SERVICE_SID: 'IS43d6dbe9027e4f8390354d306f6f4ada',
   getTwilioClient: () => twilioClient,
 };
 
